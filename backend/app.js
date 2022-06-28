501059f994ab1e2e4cc1cfbe491dfac9af03710a
Michael YVARS <michaelyvars1@gmail.com>
2022-06-28 19:16:35 +0200

    create auth routes

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const userRoutes = require('./routes/user')
+
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 const app = express()
 app.use(bodyParser.json())
+app.use('/api/auth', userRoutes)
 
 module.exports = app
---
+++ backend/controllers/user.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcrypt')
+const jsw = require('jsonwebtoken')
+const User = require('../models/User')
+
+exports.signup = (req, res, next) => {
+
+}
+
+exports.login = (req, res, next) => {
+    
+}
