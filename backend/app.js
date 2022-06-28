a46776a0739ae6545edc9e5ba41b6a96606e9ddb
Michael YVARS <michaelyvars1@gmail.com>
2022-06-28 19:09:55 +0200

    add mongodb connection

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 const app = express()
 app.use(bodyParser.json())
