484724987e588fb3bb1cd63bd08050e946380289
Michael YVARS <michaelyvars1@gmail.com>
2022-06-28 23:15:02 +0200

    create compareId middleware

---
+++ backend/middlewares/compareId.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const Post = require('../models/Post')
+
+module.exports = (req, res, next) => {
+    User.findOne({ _id: req.body.userId })
+        .then((user) => {
+            if (user.isAdmin) {
+                next()
+            }
+
+            Post.findOne({ _id: req.params.id })
+                .then((post) => {
+                    const token = req.headers.authorization.split(' ')[1]
+                    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN || 'SECRET_TOKEN')
+                    const userId = decodedToken.userId
+
+                    if (post.userId && post.userId === userId) {
+                        next()
+                    } else {
+                        res.status(403).json({ message: 'Requête non authorisée.' })
+                    }
+                })
+                .catch((error) => res.status(500).json({ error: 'Une erreur est survenue.' }))
+        })
+        .catch((error) => res.status(500).json({ error: 'Une erreur est survenue.' }))
+}
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const postController = require('../controllers/post')
 
 const auth = require('../middlewares/auth')
-//const compareId = require('../middlewares/compareId')
+const compareId = require('../middlewares/compareId')
 
 router.get('/', auth, postController.getPosts)
 router.post('/', auth, postController.addPost)
-router.put('/:id', auth, postController.modifyPost)
-router.delete('/:id', auth, postController.deletePost)
+router.put('/:id', auth, compareId, postController.modifyPost)
+router.delete('/:id', auth, compareId, postController.deletePost)
 router.post('/:id/like', auth, postController.likePost)
 
 module.exports = router
