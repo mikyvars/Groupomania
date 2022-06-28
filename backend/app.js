8da092ab70a613698bed59ef4d647aebc06b7591
Michael YVARS <michaelyvars1@gmail.com>
2022-06-28 19:16:56 +0200

    add principals headers

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,14 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${
     .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 const app = express()
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    next()
+})
+
 app.use(bodyParser.json())
 app.use('/api/auth', userRoutes)
 
