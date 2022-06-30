1acb72a76d537f7bb07200e7878765341642d07c
Michael YVARS <michaelyvars1@gmail.com>
2022-06-30 20:34:43 +0200

    create header component & style

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import './_header.scss'
+
+function Header() {
+    return (
+        <header className="header">
+            <img className="header__logo" src="/images/logo-white.svg" alt="Logo de groupomania" />
+        </header>
+    )
+}
+
+export default Header
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Header from './components/Header'
 import Home from './pages/Home'
+import './style.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
+        <Header />
         <Home />
     </React.StrictMode>
 )
