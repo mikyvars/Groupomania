946a0a9cb10230f13296a89f6cc16506ba1dbb4e
Michael YVARS <michaelyvars1@gmail.com>
2022-07-08 16:21:49 +0200

    change for enum for grade & id compare

--- backend/middlewares/compareId.js
+++ backend/middlewares/compareId.js
@@ -3,26 +3,25 @@ const User = require('../models/User')
 const Post = require('../models/Post')
 
 module.exports = (req, res, next) => {
-    console.log(req.body.userId)
-    User.findOne({ _id: req.body.userId })
-        .then((user) => {
-            if (user.isAdmin) {
+    Post.findOne({ _id: req.params.id })
+        .then((post) => {
+            const token = req.headers.authorization.split(' ')[1]
+            const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN || 'SECRET_TOKEN')
+            const userId = decodedToken.userId
+
+            if (post.userId && post.userId === userId) {
                 next()
+            } else {
+                User.findOne({ _id: userId })
+                    .then((user) => {
+                        if (user.grade == 'admin') {
+                            next()
+                        } else {
+                            res.status(403).json({ message: 'Requête non authorisée.' })
+                        }
+                    })
+                    .catch((error) => res.status(500).json({ error: 'Une erreur est survenue.' }))
             }
-
-            Post.findOne({ _id: req.params.id })
-                .then((post) => {
-                    const token = req.headers.authorization.split(' ')[1]
-                    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN || 'SECRET_TOKEN')
-                    const userId = decodedToken.userId
-
-                    if (post.userId && post.userId === userId) {
-                        next()
-                    } else {
-                        res.status(403).json({ message: 'Requête non authorisée.' })
-                    }
-                })
-                .catch((error) => res.status(500).json({ error: 'Une erreur est survenue.' }))
         })
         .catch((error) => res.status(500).json({ error: 'Une erreur est survenue.' }))
 }
--- backend/models/User.js
+++ backend/models/User.js
@@ -6,7 +6,7 @@ const userSchema = mongoose.Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false },
+    grade: { type: String, enum: ['user', 'admin'], default: 'user' },
 })
 
 userSchema.plugin(uniqueValidator)
