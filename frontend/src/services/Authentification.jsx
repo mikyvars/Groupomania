b6a4f58531fdbe6180828b242786b8ced39c8bd2
Michael YVARS <michaelyvars1@gmail.com>
2022-07-10 22:59:02 +0200

    refactor auth service

--- frontend/src/services/Authentification.jsx
+++ frontend/src/services/Authentification.jsx
@@ -1,10 +1,56 @@
+import axios from 'axios'
 import { isExpired } from 'react-jwt'
 
-export function isAuthenticated() {
-    const { token } = JSON.parse(localStorage.getItem('user')) || ''
-    return !isExpired(token)
+export const isAuthenticated = () => {
+    const { token } = getUserData()
+    return token != null && !isExpired(token)
+}
+
+export function login(data) {
+    axios
+        .post('/auth/login', data)
+        .then((response) => {
+            window.localStorage.setItem(
+                'user',
+                JSON.stringify({
+                    token: response.data.token,
+                    userId: response.data.userId,
+                    grade: response.data.grade,
+                })
+            )
+        })
+        .catch((error) => {
+            return [false, error.response.status]
+        })
+}
+
+export function signup(data) {
+    axios
+        .post('/auth/signup', data)
+        .then((response) => {
+            return [true, null]
+        })
+        .catch((error) => {
+            return [false, error.response.status]
+        })
+}
+
+export function logout() {
+    window.localStorage.removeItem('user')
+    return window.localStorage.getItem('user') == null
 }
 
 export function getUserData() {
-    return JSON.parse(localStorage.getItem('user'))
+    if (localStorage.getItem('user') == null) {
+        return false
+    }
+
+    const userParsed = JSON.parse(localStorage.getItem('user'))
+    const { token, userId } = userParsed
+
+    if ((token || userId) == null) {
+        return false
+    }
+
+    return userParsed
 }
