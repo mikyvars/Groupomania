14d3f48486dc057b0b4e8695f4f592e0125e73f6
Michael YVARS <michaelyvars1@gmail.com>
2022-07-27 14:08:32 +0200

    create getUserPosts route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const Post = require('../models/Post')
 
 exports.getUser = (req, res, next) => {
     User.findOne({ _id: req.params.id })
@@ -9,5 +10,11 @@ exports.getUser = (req, res, next) => {
                 isAdmin: user.isAdmin,
             })
         )
+        .catch((error) => res.status(500).json({ error: 'Une erreur est survenue.' }))
+}
+
+exports.getUserPosts = (req, res) => {
+    Post.find({ postedBy: req.params.id })
+        .then((posts) => res.status(200).json(posts))
         .catch((error) => res.status(500).json({ error: 'Une erreur est survenue.' }))
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/user')
 const auth = require('../middlewares/auth')
 
 router.get('/:id', auth, userController.getUser)
+router.get('/:id/posts', auth, userController.getUserPosts)
 
 module.exports = router
