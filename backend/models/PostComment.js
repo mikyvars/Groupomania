0422d4d887c87cba9edb64db86a87d50861adda7
Michael YVARS <michaelyvars1@gmail.com>
2022-07-29 18:53:05 +0200

    i'm so dubm

--- backend/models/PostComment.js
+++ backend/models/PostComment.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const postCommentSchema = mongoose.Schema({
     content: { type: String, required: true },
+    postId: { type: String, required: true },
     posted: { type: Date, default: Date.now },
     postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 })
