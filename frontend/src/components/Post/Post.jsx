41ccefd333ef2b6b2ceb0b1e2ccae359de7ce44b
Michael YVARS <michaelyvars1@gmail.com>
2022-07-29 13:33:15 +0200

    move edit function to component

---
+++ frontend/src/components/Post/Post.jsx
@@ -0,0 +1,144 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from 'react'
+import { ThreeDotsVertical, Heart, HeartFill } from 'react-bootstrap-icons'
+import { getUserData } from '../../services/Authentification'
+import PostComment from './PostComments'
+import PostEdit from './PostEdit'
+import axios from 'axios'
+import Image from 'react-bootstrap/Image'
+import Dropdown from 'react-bootstrap/Dropdown'
+import Button from 'react-bootstrap/Button'
+
+function Post({ postId, refreshData }) {
+    const [currentData, setCurrentData] = useState([])
+    const [commentsVisibility, setCommentsVisibility] = useState(false)
+    const [editVisibility, setEditVisibility] = useState(false)
+
+    const closeComments = () => setCommentsVisibility(false)
+    const closeEdit = () => setEditVisibility(false)
+
+    const fetchData = async () => {
+        try {
+            const result = await axios.get(`/post/${postId}`, {
+                headers: {
+                    Authorization: `Bearer ${getUserData().token}`,
+                },
+                data: {
+                    userId: getUserData().userId,
+                },
+            })
+
+            setCurrentData(result.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const onDelete = async () => {
+        if (window.confirm('Voulez vous supprimez cette publication?')) {
+            try {
+                const result = await axios.delete(`/post/${postId}`, {
+                    headers: {
+                        Authorization: `Bearer ${getUserData().token}`,
+                    },
+                    data: {
+                        userId: getUserData().userId,
+                    },
+                })
+
+                if (result.status === 200) {
+                    refreshData()
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+
+    const onLike = async () => {
+        try {
+            const result = await axios.post(
+                `/post/${postId}/like`,
+                {
+                    userId: getUserData().userId,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${getUserData().token}`,
+                    },
+                }
+            )
+
+            if (result.status === 200) {
+                fetchData()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    return (
+        currentData.length !== 0 && (
+            <>
+                {commentsVisibility && <PostComment postData={currentData} close={closeComments} />}
+                {editVisibility && <PostEdit postData={currentData} refresh={fetchData} close={closeEdit} />}
+                <article className="bg-light p-3 mt-2 rounded-1 border border-1" style={{ borderColor: 'silver' }}>
+                    <div className="d-flex align-items-center justify-content-between">
+                        <div>
+                            <Image
+                                src="/images/avatar.png"
+                                alt={`Photo de profil de ${currentData.postedBy.firstName} ${currentData.postedBy.lastName}`}
+                                className="rounded-circle"
+                                style={{ height: '50px' }}
+                            />
+                        </div>
+                        <div className="ms-2 flex-grow-1">
+                            <p className="text-capitalize">
+                                {currentData.postedBy.firstName} {currentData.postedBy.lastName}
+                            </p>
+                            <p className="fs-8">
+                                Publié le {new Date(currentData.posted).toLocaleDateString('fr-FR')} à {new Date(currentData.posted).toLocaleTimeString('fr-FR')}
+                            </p>
+                        </div>
+                        {(currentData.postedBy._id === getUserData().userId || getUserData().isAdmin === true) && (
+                            <div style={{ justifySelf: 'flex-end' }}>
+                                <Dropdown>
+                                    <Dropdown.Toggle bsPrefix="p-0" className="bg-transparent border-0 remove-boxShadow" aria-label="Options de la publication">
+                                        <ThreeDotsVertical style={{ cursor: 'pointer' }} className="text-dark" />
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item onClick={() => setEditVisibility(true)}>Modifier</Dropdown.Item>
+                                        <Dropdown.Item onClick={onDelete}>Supprimer</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </div>
+                        )}
+                    </div>
+                    <div className="my-3">
+                        {currentData.content} {currentData.imageUrl && <Image src={currentData.imageUrl} style={{ height: '300px', width: '100%', objectFit: 'cover' }} />}
+                    </div>
+                    <div className="d-flex align-items-center">
+                        {currentData.usersLiked.includes(getUserData().userId) ? (
+                            <Button className="bg-transparent text-dark remove-boxShadow fs-8" style={{ borderColor: 'silver', padding: '3px 6px' }} onClick={onLike}>
+                                <Heart />
+                            </Button>
+                        ) : (
+                            <Button className="bg-transparent text-dark remove-boxShadow fs-8" style={{ borderColor: 'silver', padding: '3px 6px' }} onClick={onLike}>
+                                <HeartFill className="text-danger" />
+                            </Button>
+                        )}
+                        <Button className="bg-transparent text-dark remove-boxShadow ms-1 fs-8" style={{ borderColor: 'silver', padding: '3px 6px' }} onClick={() => setCommentsVisibility(true)}>
+                            Commentaires
+                        </Button>
+                    </div>
+                </article>
+            </>
+        )
+    )
+}
+
+export default Post
---
+++ frontend/src/components/Post/PostEdit.jsx
@@ -0,0 +1,71 @@
+import { useForm } from 'react-hook-form'
+import { getUserData } from '../../services/Authentification'
+import axios from 'axios'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
+
+function PostEdit({ postData, refresh, close }) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({ mode: 'onTouched' })
+
+    const onSubmit = async (form) => {
+        const formData = new FormData()
+        formData.append('userId', getUserData().userId)
+        formData.append('content', form.content)
+        formData.append('image_delete', form.image_delete)
+        formData.append('image', form.image[0])
+
+        try {
+            const result = await axios.put(`/post/${postData._id}`, formData, {
+                headers: {
+                    Authorization: `Bearer ${getUserData().token}`,
+                    'Content-Type': 'multipart/form-data',
+                },
+            })
+
+            if (result.status === 200) {
+                refresh()
+                close()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <Modal show={true} onHide={close}>
+            <Modal.Header>Modifier la publication</Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group>
+                        <FloatingLabel label="Modifier la publication">
+                            <Form.Control as="textarea" placeholder="Modifier la publication" defaultValue={postData.content} required minLength="10" {...register('content')} />
+                            <Form.Control.Feedback type="invalid">{errors.content && errors.content.message}</Form.Control.Feedback>
+                        </FloatingLabel>
+                    </Form.Group>
+                    <Form.Group className="mt-2">
+                        <Form.Control type="file" {...register('image')} />
+                    </Form.Group>
+                    <Form.Group className="mt-2">
+                        <Form.Check label="Supprimer le fichier" disabled={postData.imageUrl === null} {...register('image_delete')} />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer className="justify-content-start">
+                <Button variant="primary" onClick={handleSubmit(onSubmit)}>
+                    Modifier
+                </Button>
+                <Button variant="danger" onClick={close}>
+                    Annuler
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+
+export default PostEdit
